1f7af31b4ffdd5f669074cdf7647bebe3905cc9c
PriyaWens <165182734+PriyaWens@users.noreply.github.com>
2024-03-27 16:07:52 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,10 @@
-function skillsMember()) {
-  console.log('I have skills')
+function skillsMember() {
+  // Get the member object
+  var member = this;
+
+  // Get the skills object
+  var skills = member.skills;
+
+  // Return the skills object
+  return skills;
 }
